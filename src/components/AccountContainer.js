919b9b25c57d61de8b435ba5f570fff2936984cb
Michael <muniu.dev@gmail.com>
2023-12-14 10:19:07 +0300

    Challenge completed:Search filtering as expected

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+  const [transactionsData, setTransactionsData] = useState([])
+  const [searchQuery, setSearchQuery] = useState("")
+  useEffect(() => {
+    fetch("http://localhost:8001/transactions?q=" + searchQuery)
+      .then((response) => response.json())
+      .then(transactions => setTransactionsData(transactions))
+  }, [searchQuery])
+  function handleSearch(event) {
+    setSearchQuery(event.target.value)
+  }
   return (
     <div>
-      <Search />
+      <Search handleSearch={handleSearch} />
       <AddTransactionForm />
-      <TransactionsList />
+      <TransactionsList transactions={transactionsData} />
     </div>
   );
 }
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function AddTransactionForm() {
+  const [transactionDate, setTransactionDate] = useState("")
+  const [transactionDescription, setTransactionDescription] = useState("")
+  const [transactionCategory, setTransactionCategory] = useState("")
+  const [transactionAmount, setTransactionAmount] = useState("")
+  function handleFormSubmit(event) {
+    fetch("http://localhost:8001/transactions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        date: transactionDate,
+        description: transactionDescription,
+        category: transactionCategory,
+        amount: transactionAmount,
+      }),
+    });
+     alert("Transaction added successfully")
+  }
   return (
     <div className="ui segment">
-      <form className="ui form">
+      <form onSubmit={handleFormSubmit} className="ui form">
         <div className="inline fields">
-          <input type="date" name="date" />
-          <input type="text" name="description" placeholder="Description" />
-          <input type="text" name="category" placeholder="Category" />
-          <input type="number" name="amount" placeholder="Amount" step="0.01" />
+          <input value={transactionDate} onChange={(event) => setTransactionDate(event.target.value)} type="date" name="date" />
+          <input value={transactionDescription} onChange={(event) => setTransactionDescription(event.target.value)} type="text" name="description" placeholder="Description" />
+          <input value={transactionCategory} onChange={(event) => setTransactionCategory(event.target.value)} type="text" name="category" placeholder="Category" />
+          <input value={transactionAmount} onChange={(event) => setTransactionAmount(event.target.value)} type="number" name="amount" placeholder="Amount" step="0.01" />
         </div>
         <button className="ui button" type="submit">
           Add Transaction
